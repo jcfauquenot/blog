e1aa8d2e6c23e39d853a823bc55179ff50a8c852
jcfauquenot <jcfauquenot@gmail.com>
2019-08-20 11:04:49 +0200

    fix app_index

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="app_index")
      * 
      */
     public function index()
