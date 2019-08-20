94d8ea39be41a87d62d20b6809e1f424f7280c8c
jcfauquenot <jcfauquenot@gmail.com>
2019-08-20 17:47:23 +0200

    final quete arborescence

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,20 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog_index")
+     * @Route("/blog/show", name="blog_index")
      * 
      */
     public function index()
     {
-        /*   return new Response(
-            '<html><body>Blog Index</body></html>'
-        ); */
-        return $this->render('index.html.twig', [
-            'owner' => 'Thomas',
-        ]);
+        return $this->render('index.html.twig');
+    }
+    /**
+     * @Route("/blog/show/{page}", requirements={"page"="[a-z0-9\-]+"},
+     * defaults = { "page":"Mon-Super-article"},
+     * name="blog_show")
+     */
+    public function show($page)
+    {
+        $page = ucwords(str_replace('-',' ',$page));
+
+        return $this->render('show.html.twig', ['page' => $page]);
     }
 }
